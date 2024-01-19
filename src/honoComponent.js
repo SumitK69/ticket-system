0143f4b60a4fb656bef3f0e3afff805627b030e2
sumit khobragade <sumitbk1010@gmail.com>
2024-01-19 19:04:53 +0530

    link changed

--- src/honoComponent.js
+++ src/honoComponent.js
@@ -3,7 +3,12 @@ import axios from "axios";
 const HonoCompoment = async () => {
   const query = { title: "this is title" };
   console.log("req_header", query);
-  const response = await axios.post("http://localhost:8787/search", query);
+  //   const response = await axios.post("http://localhost:8787/search", query);
+  const response = await axios.post(
+    "https://my-hono-app.sumitbk1010.workers.dev/search",
+    query
+  );
+
   console.log("data:", response.data);
 };
 export default HonoCompoment;
